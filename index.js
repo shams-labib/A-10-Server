065f84de09ea2440ea0cc2ec59a4566c31511e60
shams al labib <samslabib19@gmail.com>
2025-11-09 02:26:47 +0600

    server connected with mongodb

---
+++ index.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const port = 3000;
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gs1mqwb.mongodb.net/?appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("assignment-10");
+    const productCollection = db.collection("products");
+
+    app.post("/products", async (req, res) => {
+      const newUsers = req.body;
+      const result = await productCollection.insertOne(newUsers);
+      res.send(result);
+    });
+
+    app.get("/products", async (req, res) => {
+      const cursor = await productCollection.find().toArray();
+      res.send(cursor);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.use(express.json());
+app.use(cors());
+
+// assignment-10
+// aiPzPSTuB9gEDk1D
+
+app.get("/", (req, res) => {
+  res.send("This is server");
+});
+
+app.listen(port, () => {
+  console.log(`This is port ${port}`);
+});
